8f84425fa3bd3b5e7f416c11d75c4a29d876f5f8
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2023-10-27 12:28:56 +0545

    trying to fix cors attempt 6

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -13,42 +13,51 @@ const isJson = (str: string) => {
 
 // Initialize the cors middleware
 const corsMiddleware = cors({
-  origin: "*",
-  methods: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  allowedHeaders: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  origin: "*", // Replace with your allowed origins
+  methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Content-Type",
+    "Date",
+    "X-Api-Version",
+  ],
   credentials: true, // Allow credentials (cookies, HTTP authentication) to be sent cross-origin
 });
 
 export default async function getData(req: NextApiRequest, res: NextApiResponse) {
   // Apply the CORS middleware
-  corsMiddleware(req, res);
+  corsMiddleware(req, res, () => {
+    const content_key = req.headers["x-content-key"];
+    if (!content_key) {
+      return res.status(401).json({ error: "Content key not found or invalid content key" });
+    }
 
-  const content_key = req.headers["x-content-key"];
-  if (!content_key) {
-    return res.status(401).send({ error: "Content key not found or invalid content key" });
-  }
+    const query = `SELECT token from tokens WHERE token = '${content_key}'`;
 
-  const query = `SELECT token from tokens WHERE token = '${content_key}'`;
+    db.query(query)
+      .then((dbRes) => {
+        if (dbRes.rowCount < 1) return res.status(401).json({ message: "Content key not found or invalid content key" });
 
-  await db
-    .query(query)
-    .then((dbRes) => {
-      if (dbRes.rowCount < 1) return res.status(401).send({ message: "Content key not found or invalid content key" });
+        const innerQuery = `SELECT * FROM data WHERE key = '${content_key}'`;
 
-      const innerQuery = `SELECT * FROM data WHERE key = '${content_key}'`;
-
-      db.query(innerQuery)
-        .then((dbRes) => {
-          const data = dbRes.rows[0].data;
-          dbRes.rowCount > 0
-            ? res.status(200).json({ data: isJson(data) ? JSON.parse(data) : data })
-            : res.status(200).json({ data: "" });
-        })
-        .catch(() => {
-          res.status(401).json({ error: "Content key not found or invalid content key" });
-        });
-    })
-    .catch(() => {
-      res.status(401).json({ error: "Content key not found" });
-    });
+        db.query(innerQuery)
+          .then((dbRes) => {
+            const data = dbRes.rows[0].data;
+            dbRes.rowCount > 0
+              ? res.status(200).json({ data: isJson(data) ? JSON.parse(data) : data })
+              : res.status(200).json({ data: "" });
+          })
+          .catch(() => {
+            res.status(401).json({ error: "Content key not found or invalid content key" });
+          });
+      })
+      .catch(() => {
+        res.status(401).json({ error: "Content key not found" });
+      });
+  });
 }
