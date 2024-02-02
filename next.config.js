8ce1600851b6a038279317baec5a3f0f59761be5
therealrinku <rinkunited2032@gmail.com>
2024-02-02 14:06:54 +0545

    fix: update cors config

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/data",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
