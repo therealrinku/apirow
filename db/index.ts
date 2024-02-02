838c84e54cb52498ec692953ed9b3a2b38cb425f
therealrinku <rinkunited2032@gmail.com>
2024-02-02 13:59:43 +0545

    chore: update db connection logs

--- db/index.ts
+++ db/index.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 const db = new pg.Client(process.env.DB_URL);
 
 db.connect()
-  .then(() => console.log("Connected to nerd dev db."))
-  .catch(() => console.log("Nerd dev is nerdy. it won't let me connect."));
+  .then(() => console.log("Connected to Roboshare db."))
+  .catch(() => console.log("Roboshare won't let me connect."));
 
 export default db;
