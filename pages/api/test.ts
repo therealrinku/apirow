5bab7209f1e0da185293de6cbbc3c180764b4fb6
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2024-11-01 22:54:23 +0545

    feat: update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GenerateToken(req: NextApiRequest, res: NextApiResponse) {
-   console.log(req.body);
+   console.log(req.body, "webhook log");
    res.status(200).json({ working: true });
 }
