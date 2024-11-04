8cd79866b99083d6b4e0a11ea32ad83217f8d97e
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2024-11-04 18:28:11 +0545

    feat: update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GenerateToken(req: NextApiRequest, res: NextApiResponse) {
-   console.log(req.body.line_items, req.body, "log");
+   console.log(req.body.line_items, req.body.contact_email, req.body.line_items[0].properties, "log");
    res.status(200).json({ working: true });
 }
